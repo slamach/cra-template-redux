264f25e2385c272fa5f4e1ae16d877037453d6ed
Dmitry Sviridov <sviridov.dvv@gmail.com>
2022-10-10 22:02:29 +0300

    Organize project

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import App from './components/App';
+import App from './components/App/App';
 import store from './state/store';
-import './styles/global.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
