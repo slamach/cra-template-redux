3d18c8c82d58c9294bbabb05b1cc958a5a863770
Dmitrii Sviridov <sviridov.dvv@gmail.com>
2024-04-17 00:28:40 +0200

    Fix another small bugs

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { App } from './components/App/App';
+import { App } from './components/App';
 import { store } from './state/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
